a1b111389d66737208f3019756ef29a5407ae4d8
Hilel Elizarov <hilel1011@gmail.com>
2023-08-24 21:40:25 +0300

    added quote html and setup quote story with falso mocking

--- libs/client/ui-components/src/lib/Quote/quote.component.stories.ts
+++ libs/client/ui-components/src/lib/Quote/quote.component.stories.ts
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { QuoteComponent } from './quote.component';
-
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { randUuid, randQuote } from '@ngneat/falso';
+
+import { QuoteComponent } from './quote.component';
+import { IQuote } from '@qt/shared/domain';
 
 const meta: Meta<QuoteComponent> = {
   component: QuoteComponent,
@@ -11,8 +13,38 @@ const meta: Meta<QuoteComponent> = {
 export default meta;
 type Story = StoryObj<QuoteComponent>;
 
+/**
+ * Falso's `randQuoteItem` function is perfect, but it wasn't as visually
+ * pleasing to see filler text while designing. Instead, this uses
+ * legitimate English language for the to-do item, and insert a random
+ * `completed` value
+ */
+const randQuoteItem = () => {
+  // const { id, text } = randProduct();
+  return {
+    id: randUuid(),
+    text: randQuote()
+  };
+};
+
 export const Primary: Story = {
-  args: {},
+  render: (args: QuoteComponent) => ({
+    props: args,
+  }),
+  args: {
+    quote: randQuoteItem() as IQuote
+  },
+  argTypes: {
+    triggerDelete: {
+      action: 'delete',
+    },
+    triggerEdit: {
+      action: 'edit',
+    },
+    triggerToggleComplete: {
+      action: 'toggleComplete',
+    }
+  }
 };
 
 export const Heading: Story = {
--- libs/client/ui-components/src/lib/Quote/quote.component.ts
+++ libs/client/ui-components/src/lib/Quote/quote.component.ts
@@ -1,12 +1,50 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle as faCircleOutline, faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { faCheck, faPencil } from '@fortawesome/free-solid-svg-icons';
+
+import { IQuote } from '@qt/shared/domain';
 
 @Component({
   selector: 'qt-quote',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css'],
+  styleUrls: ['./quote.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuoteComponent {}
+export class QuoteComponent {
+  @Input() quote: IQuote | undefined;
+  @Output() toggleComplete = new EventEmitter<IQuote>();
+  @Output() editQuote = new EventEmitter<IQuote>();
+  @Output() deleteQuote = new EventEmitter<IQuote>();
+
+  faCheck = faCheck;
+  faCircleOutline = faCircleOutline;
+  faPencil = faPencil;
+  faTrashCan = faTrashCan;
+
+    /**
+   * Simply emit the opposite of the current completed value
+   */
+    triggerToggleComplete() {
+      this.toggleComplete.emit(this.quote);
+    }
+  
+    /**
+     * Emit the current quote data so that a service or parent component
+     * knows what to work with.
+     */
+    triggerEdit() {
+      this.editQuote.emit(this.quote);
+    }
+  
+    /**
+     * Emit the current quote data, although only the ID will most likely
+     * be needed for deletion.
+     */
+    triggerDelete() {
+      this.deleteQuote.emit(this.quote);
+    }
+}
